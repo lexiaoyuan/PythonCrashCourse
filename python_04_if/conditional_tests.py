0e02992d1aa6d8804019a3b980e2fc394c52dbea
lexiaoyuan <2775886918@qq.com>
2020-08-07 23:05:56 +0800

    update

---
+++ python_04_if/conditional_tests.py
@@ -0,0 +1,32 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("Is car == 'audi'? I predict false")
+print(car == 'audi')
+
+s1 = "String1"
+s2 = "string2"
+s3 = "string1"
+s4 = "String1"
+print(s1 == s4)
+print(s1 == s2)
+print(s1.lower() == s3)
+
+n1 = 20
+n2 = 8
+
+print(n1 == n2)
+print(n1 != n2)
+print(n1 > n2)
+print(n1 < n2)
+print(n1 >= n2)
+print(n1 <= n2)
+
+print(s1 == s4 and n1 < n2)
+print(s1 == s4 or n1 < n2)
+
+l1 = ['list1', 'list2', 'list3']
+print('list1' in l1)
+
+print('list2' not in l1)
