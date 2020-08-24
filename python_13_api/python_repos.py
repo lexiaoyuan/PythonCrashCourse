f2e7c645dbd6427004c8581d69cdd529d328dc26
lexiaoyuan <2775886918@qq.com>
2020-08-24 10:21:46 +0800

    update

--- python_13_api/python_repos.py
+++ python_13_api/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # 执行API调用并存储响应
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -15,6 +17,7 @@ print("Total repositories:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 print("Repositories returned:", len(repo_dicts))
 
+"""
 # 研究第一个仓库
 repo_dict = repo_dicts[0]
 print("\nKeys:", len(repo_dict))
@@ -37,3 +40,37 @@ for repo_dict in repo_dicts:
     print('Stars:', repo_dict['stargazers_count'])
     print('Repository:', repo_dict['html_url'])
     print("Description:", repo_dict['description'])
+ """
+
+# names, stars = [], []
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    # stars.append(repo_dict['stargazers_count'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        'xlink': repo_dict['html_url'],
+    }
+    plot_dicts.append(plot_dict)
+
+# 可视化
+my_style = LS('#333366', base_style=LCS)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python Projects on Github'
+chart.x_labels = names
+
+# chart.add('', stars)
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos.svg')
